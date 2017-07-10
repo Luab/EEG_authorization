824ef5f1bf3b7f7d7addae6167842e968269bb60
luab <Haruhi_12>
2017-07-10 16:41:48 +0300

    More datum

--- data/txt_filter.py
+++ data/txt_filter.py
@@ -1,7 +1,22 @@
 import csv
 
-with open(r'C:\Users\innopolis\Desktop\Intership BCI\OpenBCI\OpenBCI_Python-master\data\Subject3_Alpha_1_2_channels.txt') as x:
-	f = open("processed_Subject3_Alpha_1_2_channels.txt","w")
-	for row in x:
-		row2 = eval('['+row[0:-14]+']')
-		f.write(str(row2[1])+","+str(row2[2])+"\n")
+
+source_path = r'C:\Users\innopolis\Desktop\IntershipBCI\OpenBCI\OpenBCI_Python-master\data\Alpha waves\raw'
+dest_path = r'C:\Users\innopolis\Desktop\IntershipBCI\OpenBCI\OpenBCI_Python-master\data\Alpha waves'
+
+def process_openbci_raw_files(source_path,dest_path):
+	from os.path import isfile, join
+	from os import listdir
+
+	onlyfiles = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+	for name in onlyfiles:
+		with open(source_path+r'\\'+name) as x:
+			f = open(dest_path+r"\processed_"+name,"w")
+
+			for row in x:
+				if row[0] is '%':
+					pass
+				else:
+					row2 = eval('['+row[0:-14]+']')
+					f.write(str(row2[1])+","+str(row2[2])+"\n")
+
